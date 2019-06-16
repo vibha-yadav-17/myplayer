560ec8a2df26b5055ba70c29b3a15d738bafca37
vyadav477@gmail.com <vyadav477@gmail.com>
2019-06-16 21:01:08 +0530

    login

---
+++ src/components/Login.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {GoogleLogin} from 'react-google-login';
+
+
+class Login extends React.Component{
+
+    googleCallback(response){
+        if(!response || !response.accessToken){
+            alert("sorry, Google sigin has failed. Please try again")
+            return;
+        }
+        let user = {
+            token: response.accessToken,
+            name: response.profileObj.name
+        };
+
+        localStorage.setItem("user" , JSON.stringify(user)); 
+    }
+
+  
+    render(){
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className=".col-md-4 offset-md-4">
+                        <h2 className="text-danger">Login using Google</h2>
+                        <hr/>
+                        <GoogleLogin 
+                            clientId="646131656182-hklrnim2fdlv6sit5k5gk9hrpqrghn6g.
+                            apps.googleusercontent.com"
+                            onSuccess={this.googleCallback}
+                            onFailure ={this.googleCallback}
+                            buttonText="Login"
+                        />
+
+                    </div>
+
+                </div>
+
+            </div>
+        );
+    }
+}
+
+
+export default Login;
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import{BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 
 import App from './components/App.js';
+import Login from "./components/Login.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+class Home extends React.Component{
+
+    doRedirect(){
+        let loggedIn = localStorage.getItem("user");
+
+        if(loggedIn){
+            return <Redirect to="/app" />;
+        }else{
+            return <Redirect to="/login" />
+        }
+    }
+    render(){   
+        return(
+            <Router>
+                <Route path="/app" component={App} />
+                <Route path="/Login" component={Login} />
+        
+        
+               {this.doRedirect()}
+
+            </Router>
+            );
+    }
+}
+
+
+ReactDOM.render(<Home />, document.getElementById("root"));
 
 
 
